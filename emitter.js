0b98d1d404fddc19f1193d6b80e5798d09dbb892
RunningBrute <piotrekbas8@gmail.com>
2019-04-23 18:51:17 +0200

    better background

--- emitter.js
+++ emitter.js
@@ -8,13 +8,13 @@ window.onload = function()
     {
         var cr_box = document.createElement('div');
             
-        var top_position = Math.random() * 400;
+        var top_position = Math.random() * 500;
         top_position = Math.floor(top_position);
-        var left_position = Math.random() * 400;
+        var left_position = Math.random() * 500;
         left_position = Math.floor(left_position);
 
-        cr_box.style.height = "10px";
-        cr_box.style.width = "10px";
+        cr_box.style.height = "5px";
+        cr_box.style.width = "5px";
         cr_box.style.color = "blue";
         cr_box.setAttribute("id","box")
         cr_box.style.top = top_position + "px";
