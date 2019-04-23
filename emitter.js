94aba127dfb0e40fd8d822d4ce22fa405d36a86e
RunningBrute <piotrekbas8@gmail.com>
2019-04-23 18:47:10 +0200

    added new rect on click action

--- emitter.js
+++ emitter.js
@@ -2,20 +2,23 @@
 window.onload = function()
 {
     var pos = 0;
+    var con = document.getElementById('container')
 
-    var box = document.getElementById('box')
-    var t = setInterval(move, 30);
-
-    function move()
+    con.onclick = function ()
     {
-        if (pos >= 150)
-        {
-            clearInterval(t);
-        }
-        else
-        {
-            pos += 1;
-            box.style.left = pos+'px';
-        }
+        var cr_box = document.createElement('div');
+            
+        var top_position = Math.random() * 400;
+        top_position = Math.floor(top_position);
+        var left_position = Math.random() * 400;
+        left_position = Math.floor(left_position);
+
+        cr_box.style.height = "10px";
+        cr_box.style.width = "10px";
+        cr_box.style.color = "blue";
+        cr_box.setAttribute("id","box")
+        cr_box.style.top = top_position + "px";
+        cr_box.style.left = left_position + "px";
+        document.getElementById('container').appendChild(cr_box);
     }
 };
