5778f481c9a38354371688288fe6e01706b6c0ed
Piotr Basiński <piotrekbas8@gmail.com>
2019-04-24 22:42:04 +0200

    Add new particle every 0.9s

--- canvasEmitter.js
+++ canvasEmitter.js
@@ -1,4 +1,14 @@
 const canvasElem = document.getElementById('can'); 
 const ctx = canvasElem.getContext('2d'); 
 
-ctx.fillRect(25,25,5,5);
+var timer = setInterval(createParticle, 90)
+
+function createParticle()
+{
+	let x = Math.random() * 200;
+	let y = Math.random() * 150;
+	let width = 5;
+	let height = 5;
+	
+	ctx.fillRect(x, y, width, height);
+}
