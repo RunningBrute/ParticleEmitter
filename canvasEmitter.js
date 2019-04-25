0f94a714f3a8c941e058ded82f46a367793a9543
Piotr Basiński <piotrekbas8@gmail.com>
2019-04-25 20:16:02 +0200

    Created particle are stored in array

--- canvasEmitter.js
+++ canvasEmitter.js
@@ -1,14 +1,24 @@
+
 const canvasElem = document.getElementById('can'); 
 const ctx = canvasElem.getContext('2d'); 
 
-var timer = setInterval(createParticle, 90)
+var timer = setInterval(singleCreate, 90)
+
+var particles = new Array();
 
 function createParticle()
 {
-	let x = Math.random() * 200;
-	let y = Math.random() * 150;
-	let width = 5;
-	let height = 5;
+	this.x = Math.random() * 200;
+	this.y = Math.random() * 150;
+	this.width = 5;
+	this.height = 5;
 	
-	ctx.fillRect(x, y, width, height);
+	ctx.fillRect(this.x, this.y,
+				  this.width, this.height);
+}
+
+function singleCreate()
+{
+	let p = new createParticle();
+	particles.push(p)
 }
